724d8b846961b31dc9413683fa237cd7742c1e0a
Tolu Alimi <tolualimi05@gmail.com>
2021-01-20 23:03:05 -0600

    Added maxsubtreetotal

---
+++ Python/maxsubtreetotal.py
@@ -0,0 +1,21 @@
+'''
+    Given a binary tree, return the maximum sum of the subtrees
+'''
+def maxsubtreetotal(t):
+    if isinstance(t, int) or isinstance(t, float):
+        return t
+    else:
+        return max(maxsubtreetotal(t[0]), maxsubtreetotal(t[1]), total(t[0]) + total(t[1])) # max(-3,5,2) returns 5 max(2,1,3) returns 3
+
+# Returns the sum of the leaves in the binary subtree t
+def total(t):
+    if isinstance(t, int) or isinstance(t, float):
+        return t
+    else:
+        return total(t[0]) + total(t[1])
+
+print(maxsubtreetotal((-3, 4)))
+print(maxsubtreetotal(((-3, 5), (2, 1))))
+print(maxsubtreetotal((((-3, 4), 5), (2, 1))))
+
+print(total((((-3, 4), 5), (2, 1))))

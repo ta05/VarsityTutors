6d840d897f8022ed5814289d48e3e05b901f35db
Tolu Alimi <tolualimi05@gmail.com>
2021-11-19 19:58:15 -0600

    Added code program

---
+++ Python/Ilani/README.md
@@ -0,0 +1,3 @@
+# Task
+
+Write a program which prompts the user to select to encode a message or decode a message. Read the message from a file, decode/encode the message, and write the new message to an output file. Print both the original message and the encoded/decoded message.
---
+++ Python/Ilani/coder.py
@@ -0,0 +1,49 @@
+def read(filename):
+    file = open("./Ilani/files/" + filename, "r")
+    return file.read()
+
+def write(filename, message):
+    file = open("./Ilani/files/" + filename, "w")
+    file.write(message)
+
+def encode(plaintext, shift):
+    ciphertext = ""
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for character in plaintext:
+        if character in alphabet:
+            ciphertext += alphabet[alphabet.index(character) + shift]
+        else:
+            ciphertext += character
+    return ciphertext
+
+def decode(ciphertext, shift):
+    return encode(ciphertext, -1 * shift)
+
+if __name__ == "__main__":
+    while True:
+        selection = input("Would you like to encode or decode the message?\nType E to encode, D to decode, or Q to quit: ").lower()
+        if selection == "q":
+            break
+        elif selection == "e":
+            infile = input("\nPlease enter a file for reading: ")
+            plaintext = read(infile)
+            ciphertext = encode(plaintext, 3)
+            outfile = input("Please enter a file for writing: ")
+            write(outfile, ciphertext)
+
+            print("\nPlaintext:\n{}".format(plaintext))
+            print("\nCiphertext:\n{}\n".format(ciphertext))
+        elif selection == "d":
+            infile = input("\nPlease enter a file for reading: ")
+            ciphertext = read(infile)
+            plaintext = decode(ciphertext, 3)
+            outfile = input("Please enter a file for writing: ")
+            write(outfile, plaintext)
+
+            print("\nCiphertext:\n{}".format(ciphertext))
+            print("\nPlaintext:\n{}\n".format(plaintext))
+        else:
+            print("The selection was invalid.\n")
+
+
+

c4ea7cbcd7880289ff5dc0ded8511b06b11a3430
Tolu Alimi <talimi2000@gmail.com>
2022-01-20 20:58:43 -0600

    Added Connor's lab2

---
+++ Python/Connor/README.md
@@ -0,0 +1,19 @@
+# Lab 2
+
+***Problem 1*** Write the python code that prompts the user for **two integer values** and displays the result of the first number divided by the second, with exactly **three decimal places** displayed.  
+
+***Problem 2*** Write the python code that prompts the user for **two floating-point values** and displays the result of the first number divided by the second, with exactly **five decimal places** displayed.  
+
+***Problem 3*** Write the python code that prompts the user for **two floating-point values** and displays the result of the first number divided by the second, with exactly **six decimal places** displayed in **scientific notation**.  
+
+***Problem 4*** Write the python code that prompts the user to enter an upper or lower case letter and displays the corresponding Unicode encoding.  
+
+***Problem 5*** Write the python code that prompts the user for **two integer values** and displays the results when each of the following arithmetic operators are applied. For example, if the user enters the values 7 and 5, the output would be:  
+
+`7 + 5 = 12`  
+`7 - 5 = 2`  
+`7 * 5 = 35`  
+`7 / 5 = 1.40`  
+`7 // 5 = 1`  
+`7 % 5 = 2`  
+`7 ** 5 = 16,807`
---
+++ Python/Connor/lab2.py
@@ -0,0 +1,45 @@
+def prob1(num1, num2):
+    return "{:.3f}".format(num1/num2)
+
+
+def prob2(num1, num2):
+    return "{:.5f}".format(num1/num2)
+
+
+def prob3(num1, num2):
+    return "{:.6e}".format(num1/num2)
+
+
+def prob4(char):
+    return ord(char)
+
+def prob5(num1, num2):
+    return (num1 + num2, num1 - num2, num1 * num2, num1 / num2, num1 // num2, num1 % num2, num1 ** num2)
+
+
+if __name__ == "__main__":
+    n1 = int(input("Please enter a number "))
+    n2 = int(input("Please enter a number "))
+    print(prob1(n1, n2))
+
+    n1 = float(input("Please enter a number "))
+    n2 = float(input("Please enter a number "))
+    print(prob2(n1, n2))
+
+    n1 = float(input("Please enter a number "))
+    n2 = float(input("Please enter a number "))
+    print(prob3(n1, n2))
+
+    c1 = input("Please enter an upper or lower case letter ")
+    print(prob4(c1))
+
+    n1 = int(input("Please enter a number "))
+    n2 = int(input("Please enter a number "))
+    res = prob5(n1, n2)
+    print("{:,} + {:,} = {:,}".format(n1, n2, res[0]))
+    print("{:,} - {:,} = {:,}".format(n1, n2, res[1]))
+    print("{:,} * {:,} = {:,}".format(n1, n2, res[2]))
+    print("{:,} / {:,} = {:,.2f}".format(n1, n2, res[3]))
+    print("{:,} // {:,} = {:,}".format(n1, n2, res[4]))
+    print("{:,} % {:,} = {:,}".format(n1, n2, res[5]))
+    print("{:,} ** {:,} = {:,}".format(n1, n2, res[67]))

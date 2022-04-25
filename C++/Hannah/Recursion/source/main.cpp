d4c8604ea51e5216952acaa345ab9465d01bba48
Tolu Alimi <talimi2000@gmail.com>
2022-04-25 01:46:35 -0500

    Added more tests

--- C++/Hannah/Recursion/source/main.cpp
+++ C++/Hannah/Recursion/source/main.cpp
@@ -5,9 +5,17 @@ using namespace std;
 
 int main() {
     charNumTimes(5, 'X');
+    charNumTimes(3, 'o');
+    
     cout << sumDigits(54321) << endl;
+    cout << sumDigits(421) << endl;
+
     cout << rPower(2, 8) << endl;
+    cout << rPower(3, 2) << endl;
+
     fibonnaci(4);
+    fibonnaci(0);
+    fibonnaci(1);
 
     return 0;
 }

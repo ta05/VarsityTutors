0b67cef6324cc3cf6645df73dfb29441fd0382c7
Tolu Alimi <talimi2000@gmail.com>
2022-11-21 01:21:21 -0600

    Changed order of functions

--- Python/Emma/Wordle/wordle.py
+++ Python/Emma/Wordle/wordle.py
@@ -1,9 +1,6 @@
 import csv
 import random
 
-def filter_by_word_length(word_dicts: dict, word_length: int, word_type: str) -> list[dict]:
-    return [word_dict for word_dict in word_dicts if word_dict["word_length"] == word_length and word_dict["word_type"] == word_type]
-
 
 def write_word_length_files(unique_word_dicts: list[dict], word_length: int, filename: str) -> None:
     file = csv.writer(filename)
@@ -11,6 +8,10 @@ def write_word_length_files(unique_word_dicts: list[dict], word_length: int, fil
     for word_dict in unique_word_dicts:
         if word_dict["word_length"] == word_length:
             file.writerow([word_dict["word"], word_dict["word_type"]])
+
+
+def filter_by_word_length(word_dicts: dict, word_length: int, word_type: str) -> list[dict]:
+    return [word_dict for word_dict in word_dicts if word_dict["word_length"] == word_length and word_dict["word_type"] == word_type]
 
 
 def get_random_word(unique_word_dicts: list[dict]) -> list[dict]:

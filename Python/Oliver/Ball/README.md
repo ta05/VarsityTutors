9a0bd9bad1fba0b22967adf56a0514ffb4c0a93b
Tolu Alimi <talimi2000@gmail.com>
2022-12-04 19:39:51 -0600

    Added Oliver Ball program

---
+++ Python/Oliver/Ball/README.md
@@ -0,0 +1,18 @@
+# Ball
+
+Create a class called `Ball` that has two attributes, radius and color.
+
+The class should have the following methods:
+
+- `circumference`: returns the circumference of the object
+- `surface`: returns the surface of the object
+- `volume`: returns the volume of the object
+- `paint`: accepts a parameter and changes the color of the object.
+
+Write a program that will ask the user for radius and color.
+
+The program should allow the user to create more than one ball.
+
+Program should allow the user to display the circumference, surface, volume and color of all the objects when requested and the number of the objects.
+
+Test your program with different attributes.
---
+++ Python/Oliver/Ball/ball.py
@@ -0,0 +1,45 @@
+import math
+
+PI = math.pi
+
+class Ball:
+    
+    def __init__(self, radius, color):
+        self.radius = radius
+        self.color = color
+    
+    def circumference(self):
+        return 2 * PI * self.radius
+
+    def surface(self):
+        return 4 * PI * self.radius ** 2
+
+    def volume(self):
+        return PI * self.radius ** 3
+
+    def paint(self, color):
+        self.color = color
+
+
+def main():
+    # Instance variables and Constructor (init)
+    ball_1 = Ball(4, "red")
+    print("Ball 1:", ball_1.radius, ball_1.color)
+
+    ball_2 = Ball(6, "blue")
+    print("Ball 2:", ball_2.radius, ball_2.color)
+
+    # Normal methods
+    circum_1 = ball_1.circumference()
+    print("Ball 1's circumference is", circum_1)
+    print("Ball 1's surface area is", ball_1.surface())
+    print("Ball 2's volume is", ball_2.volume())
+
+    # setter method
+    print("Ball 1 is", ball_1.color)
+    ball_1.paint("purple")
+    print("Ball 1 is", ball_1.color)
+
+
+if __name__ == "__main__":
+    main()

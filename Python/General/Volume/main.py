fafe0182180775e609a068c952b242abb0de836f
Tolu Alimi <talimi2000@gmail.com>
2024-02-23 14:00:30 -0600

    Commented out extra import

--- Python/General/Volume/main.py
+++ Python/General/Volume/main.py
@@ -2,17 +2,17 @@ import cone
 # from cone import volume
 # from cone import volume as f
 # import cone as m
-from cone import *
+# from cone import *
 
 def main():
     height = float(input("Enter cone height: "))
     radius = float(input("Enter cone radius: "))
 
-    print(cone.volume(height, radius))
-    # print(volume(height, radius))
-    # print(f(height, radius))
-    # print(m.volume(height, radius))
-    # print(volume(height, radius))
+    print(f"The volume of the cone is {cone.volume(height, radius)}")
+    # print(f"The volume of the cone is {volume(height, radius)}")
+    # print(f"The volume of the cone is {f(height, radius)}")
+    # print(f"The volume of the cone is {m.volume(height, radius)}")
+    # print(f"The volume of the cone is {volume(height, radius)}")
 
 
 if __name__ == "__main__":
